f531a520bfdef497da4df4f14863b30174241382
Wout <wout@bittremieux.be>
2019-02-21 10:27:18 -0800

    Mirror plot two spectra

--- spectrum_utils/plot.py
+++ spectrum_utils/plot.py
@@ -14,7 +14,8 @@ zorders = {'a': 3, 'b': 4, 'c': 3, 'x': 3, 'y': 4, 'z': 3, 'unknown': 2,
 
 
 def spectrum(spec: MsmsSpectrum, color_ions: bool = True,
-             annotate_ions: bool = True, ax: plt.Axes = None) -> plt.Axes:
+             annotate_ions: bool = True, mirror_intensity: bool = False,
+             ax: plt.Axes = None) -> plt.Axes:
     """
     Plot an MS/MS spectrum.
 
@@ -28,6 +29,8 @@ def spectrum(spec: MsmsSpectrum, color_ions: bool = True,
     annotate_ions : bool, optional
         Flag indicating whether or not to annotate fragment ions. The default
         is True.
+    mirror_intensity : bool, optional
+        Flag indicating whether to flip the intensity axis or not.
     ax : plt.Axes, optional
         Axes instance on which to plot the spectrum. If None the current Axes
         instance is used.
@@ -48,12 +51,18 @@ def spectrum(spec: MsmsSpectrum, color_ions: bool = True,
         color = colors.get(ion_type) if color_ions else colors.get(None)
         zorder = zorders.get(ion_type)
 
-        ax.plot([mz, mz], [0, intensity / max_intensity], color=color,
-                zorder=zorder)
+        peak_intensity = intensity / max_intensity
+        if mirror_intensity:
+            peak_intensity *= -1
+        ax.plot([mz, mz], [0, peak_intensity], color=color, zorder=zorder)
 
         if annotate_ions and annotation is not None:
-            ax.text(mz + 5, intensity / max_intensity + 0.02, str(annotation),
-                    color=color, zorder=5, rotation=90, rotation_mode='anchor')
+            annotation_pos = (peak_intensity + 0.02 if not mirror_intensity
+                              else peak_intensity)
+            ax.text(mz, annotation_pos, str(annotation), color=color, zorder=5,
+                    horizontalalignment=('left' if not mirror_intensity else
+                                         'right'), verticalalignment='center',
+                    rotation=90, rotation_mode='anchor')
 
     min_mz = max(0, math.floor(spec.mz[0] / 100 - 1) * 100)
     max_mz = math.ceil(spec.mz[-1] / 100 + 1) * 100
@@ -75,5 +84,56 @@ def spectrum(spec: MsmsSpectrum, color_ions: bool = True,
 
     ax.set_xlabel('m/z')
     ax.set_ylabel('Intensity')
+
+    return ax
+
+
+def mirror(spec_top: MsmsSpectrum, spec_bottom: MsmsSpectrum,
+           color_ions: bool = True, annotate_ions: bool = True,
+           ax: plt.Axes = None):
+    """
+    Mirror plot two MS/MS spectra.
+
+    Parameters
+    ----------
+    spec_top : MsmsSpectrum
+        The spectrum to be plotted on the top.
+    spec_bottom : MsmsSpectrum
+        The spectrum to be plotted on the bottom.
+    color_ions : bool, optional
+        Flag indicating whether or not to color annotated fragment ions. The
+        default is True.
+    annotate_ions : bool, optional
+        Flag indicating whether or not to annotate fragment ions. The default
+        is True.
+    ax : plt.Axes, optional
+        Axes instance on which to plot the spectrum. If None the current Axes
+        instance is used.
+
+    Returns
+    -------
+    plt.Axes
+        The matplotlib Axes instance on which the spectra are plotted.
+    """
+    if ax is None:
+        ax = plt.gca()
+
+    # Top spectrum.
+    spectrum(spec_top, color_ions, annotate_ions, False, ax)
+    # Mirrored bottom spectrum.
+    spectrum(spec_bottom, color_ions, annotate_ions, True, ax)
+
+    ax.axhline(0, color='#9E9E9E', zorder=10)
+
+    # Update axes so that both spectra fit.
+    min_mz = max([0, math.floor(spec_top.mz[0] / 100 - 1) * 100,
+                  math.floor(spec_bottom.mz[0] / 100 - 1) * 100])
+    max_mz = max([math.ceil(spec_top.mz[-1] / 100 + 1) * 100,
+                  math.ceil(spec_bottom.mz[-1] / 100 + 1) * 100])
+    ax.set_xlim(min_mz, max_mz)
+    ax.yaxis.set_major_formatter(mticker.FuncFormatter(
+        lambda x, pos: '{:.0%}'.format(abs(x))))
+    ax.set_ylim(-1.15 if annotate_ions else -1.05,
+                1.15 if annotate_ions else 1.05)
 
     return ax

cfc5f27fbea72376de57148da3597e69f8425c8e
Wout <wout@bittremieux.be>
2021-03-31 15:46:55 -0700

    Update build configuration

--- setup.py
+++ setup.py
@@ -1,47 +1,5 @@
 import setuptools
 
-import spectrum_utils
 
-
-DISTNAME = 'spectrum_utils'
-VERSION = spectrum_utils.__version__
-DESCRIPTION = 'Mass spectrometry utility functions'
-with open('README.md') as f_in:
-    LONG_DESCRIPTION = f_in.read()
-AUTHOR = 'Wout Bittremieux'
-AUTHOR_EMAIL = 'wout.bittremieux@uantwerpen.be'
-URL = 'https://github.com/bittremieux/spectrum_utils'
-LICENSE = 'Apache 2.0'
-
-setuptools.setup(
-    name=DISTNAME,
-    version=VERSION,
-    author=AUTHOR,
-    author_email=AUTHOR_EMAIL,
-    description=DESCRIPTION,
-    long_description=LONG_DESCRIPTION,
-    long_description_content_type='text/markdown',
-    url=URL,
-    license=LICENSE,
-    platforms=['any'],
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: MacOS',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: Unix',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3 :: Only',
-        'Topic :: Scientific/Engineering :: Bio-Informatics'],
-    packages=['spectrum_utils'],
-    install_requires=[
-        'matplotlib',
-        'numba>=0.47',
-        'numpy',
-        'pyteomics'],
-    extras_require={
-        'iplot': ['altair', 'pandas']
-    }
-)
+if __name__ == '__main__':
+    setuptools.setup()
--- spectrum_utils/__init__.py
+++ spectrum_utils/__init__.py
@@ -1,3 +1,17 @@
-__version__ = '0.3.4'
+try:
+    from importlib.metadata import version, PackageNotFoundError
+
+    try:
+        __version__ = version(__name__)
+    except PackageNotFoundError:
+        pass
+except ImportError:
+    from pkg_resources import get_distribution, DistributionNotFound
+
+    try:
+        __version__ = get_distribution(__name__).version
+    except DistributionNotFound:
+        pass
+
 
 __all__ = ['iplot', 'plot', 'spectrum', 'utils']
